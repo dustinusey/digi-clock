eef90115320c063d9c3329351320a2b973fdd002
dustinusey <dustinusey@me.com>
2024-02-22 18:24:25 -0600

    digit clock with react and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import DigiClock from "./DigiClock";
+const App = () => {
+  return (
+    <div className="h-screen grid place-items-center bg-indigo-700">
+      <DigiClock />
+    </div>
+  );
+};
+export default App;
